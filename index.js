308925fd3e616c6e11338299f98f118d5dafa300
Indraacan <9haanacan@gmail.com>
2020-04-07 15:22:59 +0700

    add rain

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 //const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
 
-// fetch('')
-//     .then(response => {
-//         //console.log(response.json());
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
+
+// const addWheather = (item, nomor) => {
+//     const {
+//         weather,clouds
+//     } = item;
+
+//     const stringHTML = `${weather} ${clouds}  `;
+//     document.getElementById('wheather').innerHTML = stringHTML;
+
+// }
+
     const serverWheather = 'http://api.openweathermap.org/';
     fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
     .then(response => {
@@ -15,4 +18,17 @@
     })
     .then(data => {
         console.log(data);
+    document.getElementById('weather').innerHTML=`<div> <i class="fa fa-cloud-rain"></i> <h3>${data.weather[0].main}</h3></div>`;
+    //ocument.getElementById('weather').innerHTML=`<div> ${data.main.temp}</div>`;
     })
+    
+
+    // const weatherHTMLString = `<div class="col-sm-6 coll-md-4>
+    // weather : ${weather}
+    // <br>
+    // wind : ${wind}
+    // <br>
+    // Clouds : ${clouds}</div>`;
+    // document.getElementById('wheather').innerHTML =weatherHTMLString;
+    // 
+
