d1771ac14e06918e98125f6c9cc52a76618e6162
Indraacan <9haanacan@gmail.com>
2020-04-07 16:48:57 +0700

    add 1

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 //const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
 
-
-// const serverWheather = 'http://api.openweathermap.org/';
-// fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
-// .then(response => {
-//     return response.json();
-// })
-// .then(data => {
-//     console.log(data);
-// })
-
-
-// const addWheather = (item, nomor) => {
-//     const {
-//         weather,clouds
-//     } = item;
-
-//     const stringHTML = `${weather} ${clouds}  `;
-//     document.getElementById('wheather').innerHTML = stringHTML;
-
-// }
-
     const serverWheather = 'http://api.openweathermap.org/';
     fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
     .then(response => {
