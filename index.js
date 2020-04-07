8d57afc56497bb79b8a85de59a108cc8a8a8509b
Indraacan <9haanacan@gmail.com>
2020-04-07 16:46:52 +0700

    add new

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 //const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
 
 
-// fetch('')
-//     .then(response => {
-//         //console.log(response.json());
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-const serverWheather = 'http://api.openweathermap.org/';
-fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
+// const serverWheather = 'http://api.openweathermap.org/';
+// fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
+// .then(response => {
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data);
+// })
 
 
 // const addWheather = (item, nomor) => {
@@ -39,7 +31,7 @@ fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d5
     
     
     document.getElementById("city").innerHTML = `${data.name}`
-    document.getElementById('weather').innerHTML=`<div> <h3>${data.weather[0].main}</h3></div>`;
+    document.getElementById('weather').innerHTML=`<div> <i class="fa fa-cloud" aria-hidden="true"></i><h3>${data.weather[0].main}</h3></div>`;
     document.getElementById('temp').innerHTML=`<div> Temperature  : ${data.main.temp}</div>`;
     document.getElementById('speed').innerHTML= `<div> speed winds :${data.wind.speed} m/s</div>`
     })
