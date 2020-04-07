9d0bc2ae43df5842b730da0f6bf6a25ece80aabd
arb712 <ariqbimantoro@gmail.com>
2020-04-07 15:55:00 +0700

    update file

--- index.js
+++ index.js
@@ -8,11 +8,12 @@
 //     .then(data => {
 //         console.log(data);
 //     })
-    const serverWheather = 'http://api.openweathermap.org/';
-    fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
+const serverWheather = 'http://api.openweathermap.org/';
+fetch(`${serverWheather}data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d`)
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+})
+
